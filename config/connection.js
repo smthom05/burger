23e37b36a75b1a04cb39abd9fcb54cc2498ff6e5
smthom05@gmail.com <smthom05@gmail.com>
2018-03-12 20:34:44 -0600

    JawsDB added

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "Passw0rd!",
-  database: "burgers_db"
-});
+var connection;
+
+if (process.env.JAWSDB_URL){
+ connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+ connection = mysql.createConnection(port: 3306,
+   host: "e764qqay0xlsc4cz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+   user: "xk4otxpteirq1jck",
+   password: "hf9gpa5ry90fcm6k",
+   database: "burger_db");
+}
 
 // Make connection.
 connection.connect(function(err) {
